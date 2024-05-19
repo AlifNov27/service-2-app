62e09e49649875a5c0ba602831578df55fc436c8
AlifNov27 <alifnov7@gmail.com>
2024-05-19 14:14:55 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+res.send('Hello ini server 2!');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+   console.log(`Server listening on port ${PORT}...`);
+});
